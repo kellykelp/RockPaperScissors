29c2ec37c1507867bb8f052b265969592c21ebcf
kellykelp <kelo@s.sfusd.edu>
2018-02-04 09:02:56 -0800

    Finished.

--- build-tmp/source/RockPaperScissors.java
+++ build-tmp/source/RockPaperScissors.java
@@ -1,12 +1,28 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class RockPaperScissors extends PApplet {
+
 String s = new String();
 String instruction = new String();
 ArrayList<String> myList = new ArrayList<String>();
 String userChoice = new String();
 String compChoice = new String();
 
-void setup() {
+public void setup() {
 
-	size(300, 200);
+	
 	background(0);
 	s = "Hi! Here to play Rock, Paper, Scissors? Well, type \nin an answer and see if you can beat the computer!";
 	instruction = "Press 1 for rock! \nPress 2 for paper! \nPress 3 for scissors!";
@@ -18,11 +34,11 @@ void setup() {
 
 }
 
-void draw() {
+public void draw() {
 
 }
 
-void keyPressed() {
+public void keyPressed() {
 
 	if (key == '1')
 	{
@@ -90,4 +106,14 @@ void keyPressed() {
 		}
 	}
 
+}
+  public void settings() { 	size(300, 200); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "RockPaperScissors" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
